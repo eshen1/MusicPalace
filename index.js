2b1488fb500801c2930572073470f1af323cb962
eshen1 <eshen1@lion.lmu.edu>
2018-03-31 17:14:30 -0700

    search

--- index.js
+++ index.js
@@ -25,12 +25,64 @@ $("#buttons").click(function() {
 //     });
 // });
 
+// $('#search').click(function () {
+//     $.ajax({
+//         type: "GET",
+//         url: "https://api.spotify.com/v1/search?q=Muse%20blackout&type=track%2Cartist",
+//         dataType: "xml",
+//         beforeSend: function (xhr) {
+//             xhr.setRequestHeader('Authorization', 'Bearer' + 'BQAs1GwetZFUhWmAPZUS_RRZlPsJKFLiT2T_6ZvekdDydtxSlmmSOKXDGnNsvZVX81PEDRlpXGXTeZfAAq-bJqeu0shwh_ULGEVzMWH9pFgt6vTxD3S_p340705rRSI5OZ7FOMMopvSNILoEPJ-moJfyxIXSlCo');
+//         },
+//         success: function (xml) {
+//             console.log(xml);
+//             console.log('yes');
+//         }
+//     });
+// });
+
+// $('#search').click(function () {
+//   let clientID = '0442afbef13c432d8d617507b9ba89ab';
+//   let clientSecret = '2b100f45ee0e43b1ab61a6607238618e';
+//     $.ajax({
+//         type: "POST",
+//         url: "https://accounts.spotify.com/api/token",
+//         beforeSend: function (xhr) {
+//             xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+//             xhr.setRequestHeader('Access-Control-Allow-Credentials', 'true');
+//             xhr.setRequestHeader("Authorization", "Basic " + btoa(clientID + ':' + clientSecret));
+//         },
+//         //dataType: "json",
+//         data: {"grant_type": "client_credentials"},
+//         // success: function (xhr) {
+//         //     console.log(xhr.status);
+//         //     console.log(xhr.statusText);
+//         //     console.log(xhr.response);
+//         // }
+//
+//     });
+// });
+
 $('#search').click(function() {
+  let clientID = '0442afbef13c432d8d617507b9ba89ab';
+  let clientSecret = '2b100f45ee0e43b1ab61a6607238618e';
   let xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://ws.audioscrobbler.com/2.0/?method=track.search&track=Believe&api_key=947d8c62467598874b98abe12a96a637&format=json", false);
+  xhr.open("POST", "https://accounts.spotify.com/api/token");
+  xhr.setRequestHeader('Authorization', 'Basic' + btoa(clientID + ':' +clientSecret));
+  xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
   xhr.send();
   console.log(xhr.status);
   console.log(xhr.statusText);
   console.log(xhr.response);
 //  $('#searchresults').append(xhr.response);
 });
+
+// $('#search').click(function() {
+//   let xhr = new XMLHttpRequest();
+//   xhr.open("GET", "https://api.spotify.com/v1/search?q=Muse%20blackout&type=track", false);
+//   xhr.setRequestHeader('Authorization', 'Bearer' + 'BQAVTivzRI871w6BhJSy0yHDLjHLVNmPPnbFIoj4QlDecYv1pJlsciZaR9dKUgE5R7iGzBGcDdxOcecM2SjOdVrGqy9xLf2Zv1qv-MTFYFt2-ARunyl77DOYaNXwRIQgxOH-l5FM5XIAOtm2L2FbllHw_em_gODBhsRnA3aUdg');
+//   xhr.send();
+//   console.log(xhr.status);
+//   console.log(xhr.statusText);
+//   console.log(xhr.response);
+// //  $('#searchresults').append(xhr.response);
+// });
