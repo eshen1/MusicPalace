4cf05f366dc0dc3ce02816ee7ac71a459f059a43
jbreaker1 <jpatel8@lion.lmu.edu>
2018-04-01 17:26:53 -0700

    Progress but not complete

--- Database/user.js
+++ Database/user.js
@@ -1,17 +1,27 @@
-let neo4j = require('neo4j-driver').v1;
-let driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "12qwer"));
-let session = driver.session();
+let insertUser = function(querys) {
+  let neo4j = require('neo4j-driver').v1;
+  let driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "12qwer"));
+  let session = driver.session();
+  const query = querys;
 
-session
-  .run('CREATE (database:User {email: {emailParam}, password: {passwordParam}) RETURN database', {email: "test@gmail.com", password: "password"})
-  .subscribe({
-    onNext: function (record) {
-      console.log(record.get('name'));
-    },
-    onCompleted: function () {
-      session.close();
-    },
-    onError: function (error) {
-      console.log(error);
-    }
-  });
+  session
+    .run(query, {nameParam: 'name'})
+    .subscribe({
+      onNext: function (record) {
+        console.log(record.get('name'));
+      },
+      onCompleted: function () {
+        session.close();
+      },
+      onError: function (error) {
+        console.log(error);
+      }
+    });
+};
+
+$("#button2").click(function() {
+  let item = document.getElementById('userInfo').value;
+  let query = 'MERGE (name:Person {name : {nameParam} }) RETURN name.name AS name';
+  insertUser(query);
+  console.log("Here");
+});
