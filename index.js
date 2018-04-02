cfe4cdc04671c596b7ba592a5855d5a1c1352ec5
eshen1 <eshen1@lion.lmu.edu>
2018-04-02 00:33:41 -0700

    added pause

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ACCESS_TOKEN = 'BQAC8M0GWAR37VDtfO4DaRSHP4NkaN-Zwnd_kGsNVy8o81kJZxSgCufg30iu0YUZKyZ00FLCFZpbOP4JI2D4Y6z4eTkkSTa63XQHKwSUa925tirzV268dh5qd4oqfxIJqb8BUJ_1TGSsF3NgO-Gu5f2GSDzxMbsu141h4biUN271';
+const ACCESS_TOKEN = 'BQBmrrIBPwKGD6ph5csPpH_7WZiPsOeMzXz2CyO0MMq3YYmiWEjMiX_HwGyK_T0uOz2PpnHlg5ol9r822ZljxNu13OSpSFI68bFQ0YUdfw4yBGIyJH2le8jGSYA9sNGkh2GLTI0aS-5sSTBF-rM_KN7lkS8Y48UWxNlY8dI5UjO1';
 
 $( '.sidenav a +' ).click(function() {
   let person = prompt('Please enter your music palace Name', 'Music');
@@ -20,6 +20,60 @@ $('#buttons').click(function() {
   document.getElementById('loginpage').style.display = 'none';
 });
 
+// $(document).ready(function(){
+//     $('button').click(function(){
+//       document.getElementById('homepage').style.display = 'block';
+//     	document.getElementById('homepage').style.visibility = 'visible';
+//     });
+// });
+
+
+// $('#search').click(function () {
+//   let clientID = '0442afbef13c432d8d617507b9ba89ab';
+//   let clientSecret = '2b100f45ee0e43b1ab61a6607238618e';
+//     $.ajax({
+//         type: 'POST',
+//         url: 'https://accounts.spotify.com/api/token',
+//         headers: {
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Credentials': 'true',
+//             'Authorization': 'Basic ' + btoa(clientID + ':' + clientSecret)
+//         },
+//         dataType: 'jsonp',
+//         data: {'grant_type': 'client_credentials'},
+//         // success: function (xhr) {
+//         //     console.log(xhr.status);
+//         //     console.log(xhr.statusText);
+//         //     console.log(xhr.response);
+//         // }
+//     });
+// });
+
+// $('#search').click(function() {
+//   let clientID = '0442afbef13c432d8d617507b9ba89ab';
+//   let clientSecret = '2b100f45ee0e43b1ab61a6607238618e';
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('POST', 'https://accounts.spotify.com/api/token');
+//   xhr.setRequestHeader('Authorization', 'Basic' + btoa(clientID + ':' +clientSecret));
+//   xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+//   xhr.send();
+//   console.log(xhr.status);
+//   console.log(xhr.statusText);
+//   console.log(xhr.response);
+// //  $('#searchresults').append(xhr.response);
+// });
+
+// $('#search').click(function() {
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://api.spotify.com/v1/search?q=live&type=track&market=us&limit=10&offset=5', false);
+//   xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+//   xhr.send();
+//   // console.log(xhr.status);
+//   // console.log(xhr.statusText);
+//   // console.log(xhr.response);
+//   $('#searchresults').append(xhr.response);
+//   console.log(xhr.response);
+// });
 let trackUri = '';
 $('#searchButton').click(function () {
   let searchQuery = document.getElementById('searchTerm').value;
@@ -39,6 +93,33 @@ $('#searchButton').click(function () {
 });
 
 $('#searchresults').click(function(){
+
+  // $.ajax({
+  //     type: 'PUT',
+  //     url: 'https://api.spotify.com/v1/me/player/pause?device_id=12967c87accca1114dff634933e2bea20d79475c',
+  //     headers: {
+  //         'Authorization': 'Bearer ' + ACCESS_TOKEN,
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //     }
+  // });
+
+  // $.ajax({
+  //     type: 'PUT',
+  //     url: 'https://api.spotify.com/v1/me/player/play?device_id=12967c87accca1114dff634933e2bea20d79475c',
+  //     headers: {
+  //         'Authorization': 'Bearer ' + ACCESS_TOKEN,
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //     },
+  //     body: {
+  //       // "context_uri": "spotify:track:5bknBRjKJZ643DAN2w8Yoy"
+  //       'uris': [trackUri]
+  //     },
+  //     success: function (data) {
+  //       console.log(data);
+  //     }
+  // });
 
 fetch('https://api.spotify.com/v1/me/player/play?device_id=12967c87accca1114dff634933e2bea20d79475c', {
   method: 'PUT',
