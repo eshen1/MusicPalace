72fcbc5a84313069ae7c60f162fff1ea2563e49e
eshen1 <eshen1@lion.lmu.edu>
2018-04-07 19:24:36 -0700

    playlist names

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const ACCESS_TOKEN = 'BQBnfDnhoM_cCcKsBOyPUbCtIwEK5B1e2KIunXPzcr_LZYXGsbubKBUDZuqk-vPMe2tK5XtJ8N34uiYp1ZIOmyQsnaBJVpIo2vdPh2HKpGqD3C1EamSevA1UpLsCirxeKrRc0YVCGxMYj0HqeWLOrECTKSGHjNrREM7OJl5_H6Uo';
+const ACCESS_TOKEN = 'BQBJ0eIfSbFj-o60zNf_E2ltmG62qLDqsOSn5Vj-Ihj1BeImldPIUAdWaWSPiAHHOlp76hRHKIHLKUxpNboBUaNrGWwXocX5g8uW6R5yaiCA7blr8quZeJG8wrM0m6xboLQQJghy1fgw-nqx2OC1DdkJW5w74wva1N1ia78BWj0I';
 
-$( '.sidenav a +' ).click(function() {
+$( '#plus' ).click(function() {
   let person = prompt('Please enter your music palace Name', 'Music');
     if (person != null) {
         let node = document.createElement('div');
         let textnode = document.createTextNode(person);
         node.appendChild(textnode);
-        document.getElementById('plus').prepend(node);
+        document.getElementById('playlists').prepend(node);
     }
 });
 // <script type='text/javascript'>
@@ -19,6 +19,63 @@ $('#buttons').click(function() {
   document.getElementById('homepage').style.display = 'block';
   document.getElementById('loginpage').style.display = 'none';
 });
+
+// $(document).ready(function(){
+//     $('button').click(function(){
+//       document.getElementById('homepage').style.display = 'block';
+//     	document.getElementById('homepage').style.visibility = 'visible';
+//     });
+// });
+
+
+// $('#search').click(function () {
+//   let clientID = '0442afbef13c432d8d617507b9ba89ab';
+//   let clientSecret = '2b100f45ee0e43b1ab61a6607238618e';
+//     $.ajax({
+//         type: 'POST',
+//         url: 'https://accounts.spotify.com/api/token',
+//         headers: {
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Credentials': 'true',
+//             'Authorization': 'Basic ' + btoa(clientID + ':' + clientSecret)
+//         },
+//         dataType: 'jsonp',
+//         data: {'grant_type': 'client_credentials'},
+//         // success: function (xhr) {
+//         //     console.log(xhr.status);
+//         //     console.log(xhr.statusText);
+//         //     console.log(xhr.response);
+//         // }
+//     });
+// });
+
+// $('#search').click(function() {
+//   let clientID = '0442afbef13c432d8d617507b9ba89ab';
+//   let clientSecret = '2b100f45ee0e43b1ab61a6607238618e';
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('POST', 'https://accounts.spotify.com/api/token');
+//   xhr.setRequestHeader('Authorization', 'Basic' + btoa(clientID + ':' +clientSecret));
+//   xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+//   xhr.send();
+//   console.log(xhr.status);
+//   console.log(xhr.statusText);
+//   console.log(xhr.response);
+// //  $('#searchresults').append(xhr.response);
+// });
+
+// $('#search').click(function() {
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://api.spotify.com/v1/search?q=live&type=track&market=us&limit=10&offset=5', false);
+//   xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+//   xhr.send();
+//   // console.log(xhr.status);
+//   // console.log(xhr.statusText);
+//   // console.log(xhr.response);
+//   $('#searchresults').append(xhr.response);
+//   console.log(xhr.response);
+// });
+
+
 
 let trackUri = '';
 $('#searchButton').click(function () {
@@ -49,6 +106,7 @@ let processResults = function(response) {
         });
 
       $(albumIcon).css('margin-bottom', '10px');
+
       albumIcon.click(function(){
         trackUri = this.id
         fetch('https://api.spotify.com/v1/me/player/play?device_id=12967c87accca1114dff634933e2bea20d79475c', {
@@ -61,6 +119,13 @@ let processResults = function(response) {
         });
         console.log(this.id);
       });
+      // albumIcon.src = response.tracks.items[i].album.images[0].url;
+      // albumIcon.style.width = '100px';
+      // albumIcon.style.height = '100px';
+      // albumIcon.style.marginBottom = '10px';
+      // albumIcon.id = response.tracks.items[i].uri;
+    //  albumIcon.className = 'songIcon';
+       // $('albumIcon').addClass('songIcon');
 
       let title = response.tracks.items[i].name;
       let artist = response.tracks.items[i].artists[0].name;
