e510c4ea191c012468e8bb4f1ca94647a704e9f7
eshen1 <eshen1@lion.lmu.edu>
2018-04-08 15:21:47 -0700

    remove comments

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ $('#plus').click(function() {
     }
 });
 
-// TODO when playlist is named, create new Page
-// TODO when playlist name is clicked on, go to that page
-
 // <script type='text/javascript'>
 //     function showHomePage(homepage){
 //     document.getElementById(homepage).style.display = 'block';
@@ -26,62 +23,6 @@ $('#buttons').click(function() {
   document.getElementById('homepage').style.display = 'block';
   document.getElementById('loginpage').style.display = 'none';
 });
-
-// $(document).ready(function(){
-//     $('button').click(function(){
-//       document.getElementById('homepage').style.display = 'block';
-//     	document.getElementById('homepage').style.visibility = 'visible';
-//     });
-// });
-
-
-// $('#search').click(function () {
-//   let clientID = '0442afbef13c432d8d617507b9ba89ab';
-//   let clientSecret = '2b100f45ee0e43b1ab61a6607238618e';
-//     $.ajax({
-//         type: 'POST',
-//         url: 'https://accounts.spotify.com/api/token',
-//         headers: {
-//             'Access-Control-Allow-Origin': '*',
-//             'Access-Control-Allow-Credentials': 'true',
-//             'Authorization': 'Basic ' + btoa(clientID + ':' + clientSecret)
-//         },
-//         dataType: 'jsonp',
-//         data: {'grant_type': 'client_credentials'},
-//         // success: function (xhr) {
-//         //     console.log(xhr.status);
-//         //     console.log(xhr.statusText);
-//         //     console.log(xhr.response);
-//         // }
-//     });
-// });
-
-// $('#search').click(function() {
-//   let clientID = '0442afbef13c432d8d617507b9ba89ab';
-//   let clientSecret = '2b100f45ee0e43b1ab61a6607238618e';
-//   let xhr = new XMLHttpRequest();
-//   xhr.open('POST', 'https://accounts.spotify.com/api/token');
-//   xhr.setRequestHeader('Authorization', 'Basic' + btoa(clientID + ':' +clientSecret));
-//   xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-//   xhr.send();
-//   console.log(xhr.status);
-//   console.log(xhr.statusText);
-//   console.log(xhr.response);
-// //  $('#searchresults').append(xhr.response);
-// });
-
-// $('#search').click(function() {
-//   let xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://api.spotify.com/v1/search?q=live&type=track&market=us&limit=10&offset=5', false);
-//   xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-//   xhr.send();
-//   // console.log(xhr.status);
-//   // console.log(xhr.statusText);
-//   // console.log(xhr.response);
-//   $('#searchresults').append(xhr.response);
-//   console.log(xhr.response);
-// });
-
 
 
 let trackUri = '';
@@ -126,13 +67,6 @@ let processResults = function(response) {
         });
         console.log(this.id);
       });
-      // albumIcon.src = response.tracks.items[i].album.images[0].url;
-      // albumIcon.style.width = '100px';
-      // albumIcon.style.height = '100px';
-      // albumIcon.style.marginBottom = '10px';
-      // albumIcon.id = response.tracks.items[i].uri;
-    //  albumIcon.className = 'songIcon';
-       // $('albumIcon').addClass('songIcon');
 
       let title = response.tracks.items[i].name;
       let artist = response.tracks.items[i].artists[0].name;
