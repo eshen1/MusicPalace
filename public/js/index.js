9f4f8a923412f51a98bdc7f834e47ce7de338035
jbreaker1 <jpatel8@lion.lmu.edu>
2018-04-15 22:48:57 -0700

    added stuff to package.json and added a linter

--- public/js/index.js
+++ public/js/index.js
@@ -1,39 +1,41 @@
-const ACCESS_TOKEN = 'BQDtZgAAtLl2dnxhGHw-KCJkRKQPiEwa7BH8zV-wo21P5L2WeGH3I-wsskCpndSuZjGfR8W9QgsePc2zH3rG3fJPaGxWDhCX4xlicrtzVz6NBLczAdOOLlwhzijsRVOHc7BJcCZjVFTZqMtT89h3qDazTRkg-aWMr4coSXxKoMnl';
+const ACCESS_TOKEN = 'BQD-xbVskzfhyvaxML0SAYhSSs8nbvyTn2RtHWGtROI5l8ewBMl_WN5JtK4KUu28O4lWppGzLqNwk2JjDJSowxUXsShtZPNwc8mE3dAwIzaXjY_HjI8t9ibM7-deMqKGijc6lGydzOW1p5PIPzcKZkwW78jM03Ye1rPMhVBR1FNp';
 
-let playlistContent = {};
+const playlistContent = {};
 let trackUri = '';
 
-$('html').on('dragenter dragleave dragover drop', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
+$('html').on('dragenter dragleave dragover drop', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
 });
 
-$('#plus').click(function() {
-  let newName = prompt('Please enter your music palace Name', 'Music');
+$('#plus').click(() => {
+  const newName = prompt('Please enter your music palace Name', 'Music');
   playlistContent[newName] = [];
-  if (newName != '') {
-    let newPlaylist = $("<div/>", {
+  if (newName !== '') {
+    const newPlaylist = $('<div/>', {
       id: newName,
-      href: '#'+newName,
-      text: newName
+      href: `#, ${newName}`,
+      text: newName,
     }).droppable();
 
-    $(newPlaylist).on("drop", function(event, ui) {
+$(newPlaylist).on("drop", (event, ui) => {
       event.preventDefault();
       event.stopPropagation();
       alert('added to' + newName);
       playlistContent[newName].push(ui.draggable);
-//     event.originalEvent.dataTransfer.dropEffect = "copy";
+      //     event.originalEvent.dataTransfer.dropEffect = "copy";
     });
 
-    newPlaylist.click(function() {
+    newPlaylist.click(() => {
       $('.searchresults').empty();
-      for (let i = 0; i < playlistContent[newName].length; i++) {
-        playlistContent[newName][i].click(function(){
+      for (let i = 0; i < playlistContent[newName].length; i += 1) {
+        playlistContent[newName][i].click(() => {
           trackUri = playlistContent[newName][i][0].id;
           fetch('https://api.spotify.com/v1/me/player/play?device_id=51fdad1fd52fb7f835d3a0b18fbe835f88040bf0', {
             method: 'PUT',
-            body: JSON.stringify({ uris: [trackUri] }),
+            body: JSON.stringify({
+              uris: [trackUri]
+            }),
             headers: {
               'Content-Type': 'application/json',
               'Authorization': 'Bearer ' + ACCESS_TOKEN
@@ -54,68 +56,69 @@ $('#plus').click(function() {
 //     }
 //   </script>
 
-$('#buttons').click(function() {
+$('#buttons').click(() => {
   document.getElementById('homepage').style.display = 'block';
   document.getElementById('loginpage').style.display = 'none';
 });
 
-$('#searchButton').click(function () {
-  window.location.href='#Search';
-  let searchQuery = document.getElementById('searchTerm').value;
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.spotify.com/v1/search?q=' + searchQuery + '&type=track&market=us&limit=10&offset=5',
-        headers: {
-            'Authorization': 'Bearer ' + ACCESS_TOKEN
-        },
-        success: function (response) {
-          // trackUri = response.tracks.items[0].uri;
-          processResults(response);
-          // console.log(response);
-        }
-    });
-});
-
-let processResults = function(response) {
+const processResults = (response) => {
   $('.searchresults').empty();
-  for (let i = 0; i < 10; i++) {
-    let result = $('<div/>', {
-      id: response.tracks.items[i].uri
+  for (let i = 0; i < 10; i += 1) {
+    const result = $('<div/>', {
+      id: response.tracks.items[i].uri,
     });
 
-    let albumIcon = $('<img />',
-      { 'class': 'songIcon',
-        id: response.tracks.items[i].uri,
-        src: response.tracks.items[i].album.images[0].url,
-        width: '100px',
-        height: '100px',
-        css: {
-          'margin-bottom': '10px'
-        }
-    }).click(function(){
+    const albumIcon = $('<img />', {
+      class: 'songIcon',
+      id: response.tracks.items[i].uri,
+      src: response.tracks.items[i].album.images[0].url,
+      width: '100px',
+      height: '100px',
+      css: {
+        'margin-bottom': '10px'
+      }
+    }).click(() => {
       trackUri = this.id;
       fetch('https://api.spotify.com/v1/me/player/play?device_id=51fdad1fd52fb7f835d3a0b18fbe835f88040bf0', {
         method: 'PUT',
-        body: JSON.stringify({ uris: [trackUri] }),
+        body: JSON.stringify({
+          uris: [trackUri]
+        }),
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + ACCESS_TOKEN
+          Authorization: 'Bearer ' + ACCESS_TOKEN
         },
       });
       console.log(this.id);
     });
 
-    let title = response.tracks.items[i].name;
-    let artist = response.tracks.items[i].artists[0].name;
-    let album = response.tracks.items[i].album.name;
-    let songInfo = document.createTextNode('Title: ' + title + ', Artist: ' + artist + ', Album: ' + album + '\n');
+    const title = response.tracks.items[i].name;
+    const artist = response.tracks.items[i].artists[0].name;
+    const album = response.tracks.items[i].album.name;
+    const songInfo = document.createTextNode(`Title: ${title}, Artist: ${artist}, Album: ${album}`);
     result.append(albumIcon);
     result.append(songInfo);
     result.draggable({
-      revert: true
+      revert: true,
     });
     $('.searchresults').append(result);
     $('.searchresults').append('<br>');
   }
-
 };
+
+$('#searchButton').click(() => {
+  window.location.href = '#Search';
+  const searchQuery = document.getElementById('searchTerm').value;
+  $.ajax({
+    type: 'GET',
+    url: `https://api.spotify.com/v1/search?q=${searchQuery}&type=track&market=us&limit=10&offset=5`,
+    headers: {
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+    success: (response) => {
+      // trackUri = response.tracks.items[0].uri;
+      processResults(response);
+      // console.log(response);
+    },
+  });
+});
