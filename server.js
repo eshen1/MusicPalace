93271b291691012ba6d24d6198eda62545b45d8c
jbreaker1 <jpatel8@lion.lmu.edu>
2018-04-22 20:04:27 -0700

    Made progress on saving playlists

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', '
 const session = driver.session();
 
 const query = 'MERGE (user:User { email:{emailParam} , password: {passwordParam} } )';
+const query2 = 'MATCH (n:User) WHERE n.email = "hello" SET n.playlist = {playlistParam}';
+let response = {};
 
 // Create application/x-www-form-urlencoded parser
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -24,31 +26,65 @@ app.get('/index.html', (req, res) => {
 
 app.post('/login.html', urlencodedParser, (req, res) => {
   // Prepare output in JSON format
-  const response = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
+  response = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+  if (response.email != undefined && response.password != undefined) {
+    session
+      .run(query, { emailParam: response.email, passwordParam: response.password })
+      .subscribe({
+        onNext: (record) => {
+          console.log(record.get('name'));
+        },
+        onCompleted: () => {
+          session.close();
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      });
+
+    console.log(response);
+  }
+
+  let responses = {
+    email: req.body.email,
+    playlistParam: req.body.playlistParam
   };
 
-  session
-    .run(query, { emailParam: response.first_name, passwordParam: response.last_name })
-    .subscribe({
-      onNext: (record) => {
-        console.log(record.get('name'));
-      },
-      onCompleted: () => {
-        session.close();
-      },
-      onError: (error) => {
-        console.log(error);
-      },
-    });
+  if (responses.playlistParam != undefined) {
+    console.log("Do I get called?");
+    console.log(response);
+    console.log(responses.email + " response.email");
+    console.log(responses.playlistParam + " playlist");
 
-  console.log(response);
+    session
+      .run(query2, { playlistParam: responses.playlistParam })
+      .subscribe({
+        onNext: (record) => {
+          console.log(record.get('name'));
+        },
+        onCompleted: () => {
+          session.close();
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      });
+  }
 
   const filePath = path.join(__dirname, '/public/login.html');
   res.sendFile(filePath);
 });
 
-app.listen(3000, function() {
-  console.log('Listening on port ', 3000);
+app.put('/login.html', urlencodedParser, (req, res) => {
+  // Prepare output in JSON format
+
+  console.log(response);
+  res.sendFile(filePath);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on', 3000);
 });
